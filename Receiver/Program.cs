dd6816864c6469bf1464cce8f7869ff3f17c6412
krishnakanth Dasi <krishnakanthdasi@gmail.com>
2021-09-21 12:26:19 +0530

    Add project files.

---
+++ Receiver/Program.cs
@@ -0,0 +1,20 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new ResponseSocket("@tcp://localhost:5500");
+
+            string message = socket.ReceiveFrameString();
+            Console.WriteLine("Message : " + message);
+
+            
+
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var socket = new RequestSocket(">tcp://localhost:5500");
+            Console.WriteLine("Message : ");
+            string message = Console.ReadLine();
+            socket.SendFrame(message);
+
+            
+             
+        }
+    }
+}
